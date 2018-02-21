1574e96e55f185c63bb798d45c6f565f7d926066
Douwe Postmus <d.postmus@umcg.nl>
2018-02-21 19:00:13 +0100

    direchlet-choice-MLE

--- dirichlet-choice-model.R
+++ dirichlet-choice-model.R
@@ -25,13 +25,14 @@ loglik.question <- function(alpha,meas,selected,N=1e2) {
   
   total.values <- weights %*% t(meas)
   prob.selected <- sum(aaply(total.values,1,which.max)==which.max(selected))/N
-  log(max(prob.selected,(1/N)^2)) # Log-likelihood contribution
+  log(max(prob.selected,1e-12)) # Log-likelihood contribution
   
 }
 
+# Fixed effects model
 loglik.survey <- function(alpha,meas,selected,question.id,N=1e2) {
   
-  set.seed(sum(alpha))
+  set.seed(prod(alpha))
   
   loglik.sum <- 0
   for (question in unique(question.id)) {
@@ -42,50 +43,61 @@ loglik.survey <- function(alpha,meas,selected,question.id,N=1e2) {
   
 }
 
-loglik.sample <- function(alpha,meas,selected,question.id,subject.id,N=1e2) {
+# Random effects model
+loglik.sample.re <- function(theta,meas,selected,question.id,subject.id,n.outer=10,n.inner=10) {
   
-  set.seed(sum(alpha))
-  n.subjects <- length(unique(subject.id))
+  set.seed(prod(theta))
+  
+  # theta <- c(alpha,C)
+  alpha <- theta[1:(length(theta)-1)]
+  C <- theta[length(theta)]
   
   loglik.sum <- 0
   for (subject in unique(subject.id)) {
-    weights <-rdirichlet(N,alpha)
-    total.values <- weights %*% t(meas)
-    prob.selected <- rep(1,N)
-    for (question in unique(question.id[subject.id==subject])) {
-      prob.selected <- prob.selected*as.numeric(aaply(total.values[,question.id==question & subject.id==subject],1,which.max)==which.max(selected[question.id==question & subject.id==subject]))
+    meas.subject <- meas[subject.id==subject,]
+    selected.subject <- selected[subject.id==subject]
+    question.id.subject <- question.id[subject.id==subject]
+    prob.selected.subject <- c()
+    for (i.outer in 1:n.outer) {
+      alpha.inner <- C*as.vector(rdirichlet(1,alpha)) 
+      prob.selected.iter <- 1
+      for (question in unique(question.id.subject)) {
+        weights <- rdirichlet(n.inner,alpha.inner)
+        total.values <- weights %*% t(meas.subject[question.id.subject==question,])
+        prob.selected <-sum(aaply(total.values,1,which.max)==which.max(selected.subject[question.id.subject==question]))/n.inner
+        prob.selected.iter <- prob.selected.iter*prob.selected
+      }
+      prob.selected.subject <- c(prob.selected.subject,prob.selected.iter)
     }
-    loglik.sum <- loglik.sum + log(max(mean(prob.selected),(1/N)^2)) # Log-likelihood contribution
-  }
-  
+    loglik.sum <- loglik.sum + log(max(mean(prob.selected.subject),1e-12)) # Log-likelihood contribution
+  } 
+   
   loglik.sum
   
 }
-
 
 ###############
 
 PVFs <- list(pfs=partialValue(90,50),mod=partialValue(45,85),sev=partialValue(20,80))
 
 source("load.dce.R")
-df <- df[df$url %in% unique(df$url)[1:10],]
+df <- df[df$url %in% unique(df$url)[1:5],]
 #df <- df[df$url=="00ncn5es",]
 
-meas.full <- data.frame(pfs=sapply(df$level.PFS,PVFs[["pfs"]]),mod=sapply(df$level.mod,PVFs[["mod"]]),sev=sapply(df$level.sev,PVFs[["sev"]])) 
-selected.full <- df$selected.by.subject
+meas <- data.frame(pfs=sapply(df$level.PFS,PVFs[["pfs"]]),mod=sapply(df$level.mod,PVFs[["mod"]]),sev=sapply(df$level.sev,PVFs[["sev"]])) 
+selected <- df$selected.by.subject
 question.id <- df$idx
 subject.id <- df$url
 
-alpha <- c(1,1,1)
+alpha <- c(2,2,2)
+theta <- c(1,1,1,5)
 
-loglik.survey(alpha,meas.full,selected.full,question.id)
-
-constrOptim(theta=alpha,f=loglik.survey,grad=NULL,ui=diag(3),ci=rep(0,3),meas=meas.full,selected=selected.full,question.id=question.id,
+loglik.sample.re(theta,meas,selected,question.id,subject.id,n.outer=10,n.inner=10)
+constrOptim(theta=theta,f=loglik.sample.re,grad=NULL,ui=diag(4),ci=rep(0,4),meas=meas,selected=selected,question.id=question.id,subject.id=subject.id,
             control=list(fnscale=-1,trace=1))
-
-loglik.sample(alpha,meas.full,selected.full,question.id,df$url)
-
-constrOptim(theta=alpha,f=loglik.sample,grad=NULL,ui=diag(3),ci=rep(0,3),meas=meas.full,selected=selected.full,question.id=question.id,subject.id=subject.id,
+  
+loglik.survey(alpha,meas.full,selected.full,question.id)
+constrOptim(theta=alpha,f=loglik.survey,grad=NULL,ui=diag(3),ci=rep(0,3),meas=meas,selected=selected,question.id=question.id,
             control=list(fnscale=-1,trace=1))
 
 
